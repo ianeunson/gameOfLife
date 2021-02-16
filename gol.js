8dcb7e5be4cdec4ff12cfc2d886f5503f8c51940
Ian Eunson <ianeunson@outlook.com>
2021-02-15 19:24:21 -0600

    adding gameplan

---
+++ gol.js
@@ -0,0 +1,30 @@
+
+
+// RULES 
+// BIRTH RULE: 
+// an empty, or "dead" cell with precisely 3 live neightbors becomes live
+
+// DEATH RULE:
+// A live cell with zero or one neightbors dies of isolation: a live cell with four or more neighbors dies of overcrowding
+
+// SURVIVAL RULE:
+// A live cell with two or three neighbors remains alive
+
+// Draw grid
+
+// Make each cell toggle-able
+
+// For each cell, check rules, 
+// (Are rules checked on all cells first then applied or applied as they go?)
+// How could I 
+
+
+
+// TODO 
+// Add speed control
+
+// Add scale control
+
+// Add customizations such as colors?
+
+// Add prebuilt objects
