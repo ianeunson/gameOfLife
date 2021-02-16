e8f0cdacd022f4adecd6cf3d057e2c27862e4b8f
Ian Eunson <ianeunson@outlook.com>
2021-02-15 19:57:34 -0600

    revised rules from wikipedia

--- gol.js
+++ gol.js
@@ -1,22 +1,26 @@
 
 
 // RULES 
-// BIRTH RULE: 
-// an empty, or "dead" cell with precisely 3 live neightbors becomes live
+// 1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+// 2. Any live cell with two or three live neighbours lives on to the next generation.
+// 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+// 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
-// DEATH RULE:
-// A live cell with zero or one neightbors dies of isolation: a live cell with four or more neighbors dies of overcrowding
+// These rules, which compare the behavior of the automaton to real life, can be condensed into the following:
 
-// SURVIVAL RULE:
-// A live cell with two or three neighbors remains alive
+// 1. Any live cell with two or three live neighbours survives.
+// 2. Any dead cell with three live neighbours becomes a live cell.
+//  (if neighbors === 3)
+// 3. All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+
 
 // Draw grid
 
 // Make each cell toggle-able
 
 // For each cell, check rules, 
-// (Are rules checked on all cells first then applied or applied as they go?)
-// How could I 
+
+
 
 
 
@@ -27,4 +31,6 @@
 
 // Add customizations such as colors?
 
-// Add prebuilt objects
+// Add prebuilt
+
+// New cells can be a combination of the colors of its' neighbors?
